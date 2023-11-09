0e0a96d831c79ca4ae19935ab7f23986230426eb
榎橘征爾 <kakitsuseiji@enokitaijinoAir.elecom>
2023-11-09 22:11:26 +0900

    a

--- my-app-ts/src/routes/sidecontents/FetchUsers.tsx
+++ my-app-ts/src/routes/sidecontents/FetchUsers.tsx
@@ -12,18 +12,18 @@ const FetchUsers = async () => {
       },
     });
 
-      if (getResponse.status === 200) {
-        // GETリクエストの結果を処理
-        const userData = await getResponse.json();
-        userData(userData);
-        // userDataを適切に処理するコードをここに追加
-      } else {
-        // GETリクエストが失敗した場合の処理
-        console.error("GET request failed");
-      }
-    } catch (err) {
-      console.error(err)
-    }
+    if (getResponse.status === 200) {
+      // GETリクエストの結果を処理
+      const userData = await getResponse.json();
+      userData(userData);
+      // userDataを適切に処理するコードをここに追加
+    } else {
+      // GETリクエストが失敗した場合の処理
+      console.error("GET request failed");
+    };
+  } catch (err) {
+    console.error(err)
   };
+};
 
 export default FetchUsers;
