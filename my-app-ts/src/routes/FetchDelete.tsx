3d2101cc9ca17426b4968567bfa4d96323a0d65e
榎橘征爾 <kakitsuseiji@enokitachibanaseijinoMacBook-Air.local>
2023-11-12 03:03:30 +0900

    2023年 11月12日 日曜日 03時03分30秒 JST

--- my-app-ts/src/routes/FetchDelete.tsx
+++ my-app-ts/src/routes/FetchDelete.tsx
@@ -1,4 +1,38 @@
+import { useEffect, useState } from "react";
+type UserData = {
+    id : string;
+    curriculum : string;
+    category : string;
+    title : string;
+    body : string;
+    datetime_column : string;
+  }
+
 export const FetchDelete = async (id : string) => {
+    const [userData, setUserData] = useState<UserData[]>([]);
+    const fetchUsers = async () => {
+        try{
+          const getResponse = await fetch("https://hackathon-backend-zjgwehekya-uc.a.run.app/user", {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+    
+          if (getResponse.status === 200) {
+            // GETリクエストの結果を処理
+            const userData = await getResponse.json();
+            setUserData(userData);
+            // userDataを適切に処理するコードをここに追加
+          } else {
+            // GETリクエストが失敗した場合の処理
+            console.error("GET request failed");
+          }
+        } catch (err) {
+          console.error(err)
+        }
+      };
+    
     try {
         const response = await fetch (`https://hackathon-backend-zjgwehekya-uc.a.run.app/user?id=${id}`, {
             method: "DELETE"
@@ -12,4 +46,8 @@ export const FetchDelete = async (id : string) => {
     } catch(err) {
         console.error(err)
     };
+
+    useEffect(() => {
+        fetchUsers();
+      },[]);
 };
